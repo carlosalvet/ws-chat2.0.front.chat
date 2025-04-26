6a0d19d7f3bf12e332718092584fce3ed37ec4b2
Carlos Sánchez <carlos.sanchez@edomex.gob.mx>
2025-04-25 19:05:50 -0600

    [ADD]init_chat Funcionalidad Basica, solo cambia status en barra estado de Chat

---
+++ chat/functionalities/callbacks.js
@@ -0,0 +1,13 @@
+async function init_chat(chat_id){
+    let request_id = 0
+    var ws = null
+    try {
+        ws_chat = new WS_ChatController("ws-chat").__init__();
+        ws_chat.set_status('Conectando...')
+    } catch (_error){
+        console.error("Error crítico:", _error);
+    }
+
+    return ws
+}
+
