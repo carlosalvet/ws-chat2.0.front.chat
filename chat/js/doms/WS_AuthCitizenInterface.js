56c366281b1fb3a508e4b2171946b0f2a739e838
Carlos Sánchez <carlos.sanchez@edomex.gob.mx>
2025-04-28 09:47:05 -0600

    [ADD]Agregar dom faltante. (no se bien a que se refiera)

---
+++ chat/js/doms/WS_AuthCitizenInterface.js
@@ -0,0 +1,11 @@
+WS_AuthCitizenInterface = {
+  'container':document.getElementById('auth-citizen-interface'),
+
+  'hide':function(){
+    this.dom_container.display = 'none'
+  },
+
+  'show':function(){
+    this.dom_container.display = ''
+  }
+}
