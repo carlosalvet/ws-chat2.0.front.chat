483fb458eb06fc35af51ae3c6bd1a0e311df6d6d
Carlos Sánchez <carlos.sanchez@edomex.gob.mx>
2025-04-28 09:50:39 -0600

    [ADD]doms/WS_AuthExpertInterface agregando este archivo faltanate par aser user.login

---
+++ chat/js/doms/WS_AuthExpertInterrface.js
@@ -0,0 +1,11 @@
+WS_AuthExpertInterface = {
+  'container':document.getElementById('auth-expert-interface'),
+
+  'hide':function(){
+    this.dom_container.display = 'none'
+  },
+
+  'show':function(){
+    this.dom_container.display = ''
+  }
+}
